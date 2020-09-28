021fd63b71fb2b1bba493fd0c521368aa49fe16e
Jerud-Moyer <jerudmoyer@gmail.com>
2020-09-28 15:44:13 -0700

    first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,24 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
+
 describe('tardygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('signs up a user via post', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com'
+    });
+  });
+
 });
+
