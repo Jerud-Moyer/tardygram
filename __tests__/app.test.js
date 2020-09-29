1535b36a06a79589dcd8341f7e062c9a5977b6ec
Jerud-Moyer <jerudmoyer@gmail.com>
2020-09-28 17:08:02 -0700

    cookie parser good

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -42,5 +42,30 @@ describe('tardygram routes', () => {
     });
   });
 
+  it('verifies a user via GET', async() => {
+    const agent = request.agent(app);
+    await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      });
+    const response = await agent
+      .get('/api/v1/auth/verify');
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com'
+    });
+    
+    const responseWithoutAUser = await request(app)
+      .get('/api/v1/auth/verify');
+
+    expect(responseWithoutAUser.body).toEqual({
+      status: 500,
+      message: 'jwt must be provided'
+    });
+  });
+
 });
 
